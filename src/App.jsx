21406a8e30ce7cda3c6ac94a5635af2c38e9bc64
GintareSlusnyte <gintareslusnyte@gmail.com>
2023-05-15 10:31:40 +0300

    Footer'io pagrindas su ikonom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './components/Organisms/Footer';
 import Header from './components/Organisms/Header';
 
 const App = () => {
   return (
     <>
       <Header />
+
+
+      <Footer />
     </>
   );
 }
--- src/components/Organisms/Footer.jsx
+++ src/components/Organisms/Footer.jsx
@@ -1,8 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTiktok, faInstagram, faTwitter, faFacebook} from '@fortawesome/free-brands-svg-icons';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+    padding: 20px 50px;
+    border-top: 2px solid black;
+    height: 100px;
+    .copyright{
+        font-weight: 400;
+        font-size: 13px;
+    }
+
+`;
+
 const Footer = () => {
     return ( 
-        <footer>
-            
-        </footer>
+        <StyledFooter>
+            <div className='copyright'>
+                ©2023 by create_react_app.com, Inc.
+            </div>
+            <NavLink to="https://www.tiktok.com/"><FontAwesomeIcon icon={faTiktok} /></NavLink>
+            <NavLink to="https://www.instagram.com/"><FontAwesomeIcon icon={faInstagram} /></NavLink>
+            <NavLink to="https://www.twitter.com/"><FontAwesomeIcon icon={faTwitter} /></NavLink>
+            <NavLink to="https://www.facebook.com/"><FontAwesomeIcon icon={faFacebook} /></NavLink>
+        </StyledFooter>
      );
 }
  
